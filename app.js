f33ff3e62669004e1296d510b03b2c1921483be8
rmalolin <rmalolin@gmail.com>
2021-08-17 16:24:04 +0300

    js smooth scroll

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector("#navbar__logo");
 
 const mobileMenu = () => {
   menu.classList.toggle("is-active");
@@ -7,3 +8,45 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener("click", mobileMenu);
+
+const highlightMenu = () => {
+  const elem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const servicesMenu = document.querySelector("#services-page");
+  let scrollPosition = window.scrollY;
+
+  if (window.innerWidth > 960 && scrollPosition < 600) {
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPosition < 1400) {
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    servicesMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPosition < 2345) {
+    servicesMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  }
+
+  if ((elem && window.innerWidth < 960 && scrollPosition < 600) || elem) {
+    elem.classList.remove("highlight");
+  }
+};
+
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
+
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector(".is-active");
+
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
+  }
+};
+
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
